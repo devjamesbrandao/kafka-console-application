673cb09856be7a8baa7a14bb8f525fe3a2220570
James Noleto <muricijames@gmail.com>
2022-08-06 22:37:50 -0300

    feat: command parser config

---
+++ ConsoleApp1/Options.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+namespace Kafka
+{
+    public class Options
+    {
+        [Option('r', "requests", Default = 1, Required = false)]
+        public int requisicoes { get; set; }
+
+        [Option('h', "host", Default = "localhost", Required = false)]
+        public string host { get; set; }
+    }
+}
